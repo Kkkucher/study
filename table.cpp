201a7b9f388e76a865769a7d4a06ca8e793ea471
Damir Magomedov <damir.magomedov2007@gmail.com>
2025-10-09 08:58:04 +0300

    add table program and add one world to the text folder

---
+++ table.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    string name;
+    int age;
+    double grade;
+};
+
+int main() {
+    Student students[] = {
+        {"Иванов", 20, 4.5},
+        {"Петров", 19, 3.8},
+        {"Сидорова", 21, 4.9},
+        {"Козлов", 20, 3.2}
+    };
+
+    int count = sizeof(students) / sizeof(students[0]);
+
+    cout << "Таблица студентов:" << endl;
+    cout << endl;
+
+    // Заголовок таблицы
+    cout << setw(15) << left << "Фамилия"
+        << setw(10) << "Возраст"
+        << setw(10) << "Оценка" << endl;
+
+    // Разделитель
+    cout << string(35, '-') << endl;
+
+    // Данные
+    for (int i = 0; i < count; i++) {
+        cout << setw(15) << left << students[i].name
+            << setw(10) << students[i].age
+            << setw(10) << fixed << setprecision(1) << students[i].grade << endl;
+    }
+
+    return 0;
+}
